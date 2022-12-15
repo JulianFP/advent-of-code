347a68a30de456518754b9bc8fb601217a7328b0
Julian Partanen <julian@partanengroup.de>
2022-12-15 12:39:50 +0100

    add solutions day13 part 2

--- day13/main.cc
+++ day13/main.cc
@@ -4,12 +4,13 @@
 #include <vector>
 #include <array>
 #include <string>
-#include <cmath>
+#include <algorithm>
 
 //0: no decision, 1: right order, -1: wrong order
 short checkPacketOrder(std::vector<int>::iterator leftIt, const std::vector<int>::iterator left_end, std::vector<int>::iterator rightIt, const std::vector<int>::iterator right_end){
     while(true){
         //check if a list has run out of items
+        if((leftIt == left_end) && (rightIt == right_end)) return 0;
         if((leftIt == left_end) && (rightIt != right_end)) return 1;
         else if((rightIt == right_end) && (leftIt != left_end)) return -1; 
 
@@ -80,7 +81,7 @@ short checkPacketOrder(std::vector<int>::iterator leftIt, const std::vector<int>
 }
 
 bool comparePackets(std::vector<int> left, std::vector<int> right){
-    short returnVal = checkPacketOrder(left.begin(), left.end()-1, right.begin(), right.end()-1);
+    short returnVal = checkPacketOrder(left.begin()+1, left.end()-1, right.begin()+1, right.end()-1);
     if(returnVal == -1) return 0;
     else return 1;
 }
@@ -91,18 +92,17 @@ int main(){
     std::cin >> filename;
     std::ifstream file;
     file.open(filename);
-    std::vector<int> oldVector, newVector;
+    std::vector<int> currentVector;
+    std::vector<std::vector<int>> packets;
     std::string current_line;
     char c;
-    int pair = 0, result = 0;
     while(!file.eof()){
         std::getline(file, current_line);
         if(current_line.empty()) continue;
         std::stringstream line(current_line);
         int current = 0;
         int iterator = 0;
-        oldVector = newVector;
-        newVector.resize(0);
+        currentVector.resize(0);
         while(line >> c){
             if(std::isdigit(c)){
                 current *= 10;
@@ -110,22 +110,40 @@ int main(){
                 ++iterator;
             }
             else if(iterator != 0){
-                newVector.push_back(current); 
+                currentVector.push_back(current); 
                 current = 0;
                 iterator = 0;
             }
-            if(c == '[') newVector.push_back(-1);
-            else if(c == ']') newVector.push_back(-2);
+            if(c == '[') currentVector.push_back(-1);
+            else if(c == ']') currentVector.push_back(-2);
             else continue;
         }
-        ++pair;
-        if(pair%2 == 0){
-            bool right_order = comparePackets(oldVector, newVector);
-            result += (pair/2)*right_order;
-            std::cout << (pair/2) << ". pair of packets: " << right_order << std::endl;
-        }
+        packets.push_back(currentVector);
+    }
+    file.close();
+    int result = 0;
+    for(int i = 0; i < packets.size(); i+=2){
+        bool right_order = comparePackets(packets[i], packets[i+1]);
+        result += ((i/2)+1)*right_order;
+        std::cout << (i/2)+1 << ". pair of packets: " << right_order << std::endl;
     }
     std::cout << "The solution for part 1 is: " << result << std::endl;
-    file.close();
+
+    //part 2
+    std::vector<int> divider_1 = {-1, -1, 2, -2, -2};
+    std::vector<int> divider_2 = {-1, -1, 6, -2, -2};
+    packets.push_back(divider_1);
+    packets.push_back(divider_2);
+    std::sort(packets.begin(), packets.end(), comparePackets);
+    int result_2 = 1;
+    for(int i = 0; i < packets.size(); i++){
+        if(packets[i] == divider_1) result_2 *= (i+1);
+        if(packets[i] == divider_2) result_2 *= (i+1);
+        for(int &entry : packets[i]){
+            std::cout << entry << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << "The solution for part 2 is: " << result_2 << std::endl;
     return 0;
 }
